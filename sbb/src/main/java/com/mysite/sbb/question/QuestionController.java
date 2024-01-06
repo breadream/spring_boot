32dd45852456a449cb348c63f939d6c6c886edbf
Brad Lim <brad.dh.lim@gmail.com>
2024-01-05 22:56:07 -0800

    Add styling

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,5 +1,7 @@
 package com.mysite.sbb.question;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -9,20 +11,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 	
 	private final QuestionService questionService;	
 
-	@GetMapping("/question/list")
+	@GetMapping("/list")
 	public String list(Model model) {
 		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList);
 		return "question_list";
 	}
 	
-	@GetMapping(value = "/question/detail/{id}")
+	@GetMapping(value = "/detail/{id}")
 	public String detail(Model model, @PathVariable("id") Integer id) {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
